f89fbaa4bac5d8c88ec28ce796cd52054bf4b224
Tomas <semaska.tomas@gmail.com>
2015-10-24 11:39:48 +0300

    added setters and getters to user class

--- src/BasketPlanner/Bundle/UserBundle/Entity/User.php
+++ src/BasketPlanner/Bundle/UserBundle/Entity/User.php
@@ -23,14 +23,28 @@ class User extends BaseUser
      *
      * @ORM\Column(name="facebook_id", type="string", nullable=true)
      */
-    private $facebookID;
+    private $facebookId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="facebook_access_token", type="string", length=255, nullable=true)
+     */
+    protected $facebook_access_token;
 
     /**
      * @var string
      *
      * @ORM\Column(name="google_id", type="string", nullable=true)
      */
-    private $googleID;
+    private $googleId;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="google_access_token", type="string", length=255, nullable=true)
+     */
+    protected $google_access_token;
 
     public function __construct()
     {
@@ -46,6 +60,98 @@ class User extends BaseUser
     public function getId()
     {
         return $this->id;
+    }
+
+    /**
+     * Get facebook Id
+     *
+     * @return string
+     */
+    public function getFacebookId()
+    {
+        return $this->facebookId;
+    }
+
+    /**
+     * Set facebook Id
+     *
+     * @param string $facebookId
+     *
+     * @return User
+     */
+    public function setFacebookID($facebookId)
+    {
+        $this->facebookId = $facebookId;
+        return $this;
+    }
+
+    /**
+     * Get google Id
+     *
+     * @return string
+     */
+    public function getGoogleId()
+    {
+        return $this->googleId;
+    }
+
+    /**
+     * Set google Id
+     *
+     * @param string $googleId
+     *
+     * @return User
+     */
+    public function setGoogleId($googleId)
+    {
+        $this->googleId = $googleId;
+        return $this;
+    }
+
+    /**
+     * Get facebook Access Token
+     *
+     * @return string
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebook_access_token;
+    }
+
+    /**
+     * Set facebook Access Token
+     *
+     * @param string $facebookAccessToken
+     *
+     * @return User
+     */
+    public function setFacebookAccessToken($facebookAccessToken)
+    {
+        $this->facebook_access_token = $facebookAccessToken;
+        return $this;
+    }
+
+    /**
+     * Get google Access Token
+     *
+     * @return string
+     */
+    public function getGoogleAccessToken()
+    {
+        return $this->google_access_token;
+    }
+
+    /**
+     * Set google Access Token
+     *
+     * @param string $googleAccessToken
+     *
+     * @return User
+     */
+    public function setGoogleAccessToken($googleAccessToken)
+    {
+        $this->google_access_token = $googleAccessToken;
+        return $this;
     }
 
 }
