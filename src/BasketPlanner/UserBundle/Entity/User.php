c8936ff7fe776a6b038c0e7a320618bd7ddb5c00
Šarūnas Jonušas <jonusas.sarunas@gmail.com>
2015-11-18 13:15:05 +0200

    fixed User entity

--- src/BasketPlanner/UserBundle/Entity/User.php
+++ src/BasketPlanner/UserBundle/Entity/User.php
@@ -4,7 +4,7 @@ namespace BasketPlanner\UserBundle\Entity;
 
 use BasketPlanner\MatchBundle\Entity\Match;
 use Doctrine\Common\Collections\ArrayCollection;
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
