ed44394985977d6723710581dc4e3069d66c5d44
Sedvis <sedvis@gmail.com>
2015-11-28 15:54:19 +0200

    Merge branch 'master' of C:\Users\Sedvis\PhpstormProjects\basketplanner with conflicts.

--- src/BasketPlanner/MatchBundle/Services/GoogleMapLoaderService.php
+++ src/BasketPlanner/MatchBundle/Services/GoogleMapLoaderService.php
@@ -54,4 +54,43 @@ class GoogleMapLoaderService{
 
         return $map;
     }
+    /**
+     * @var Ivory\GoogleMapBundle\Model\Map
+     * @var boolean $type   Should contain a boolean value
+     */
+    public function loadAddress($type){
+
+        $map = $this->container->get('ivory_google_map.map');
+
+        $map->setCenter(54.9120769, 23.9515808, true);
+        $map->setMapOption('zoom', 10);
+        $map->setMapOption('disableDefaultUI', false);
+
+        $em = $this->container->get('doctrine')->getManager()
+            ->getRepository('BasketPlannerMatchBundle:Court')->findByApproved(true);
+
+        foreach($em as $court){
+            $marker = new Marker();
+            $marker->setPosition($court->getLatitude(), $court->getLongitude(), true);
+            $marker->setOption('clickable', true);
+            $marker->setOption('markerID', $court->getId());
+            $marker->setOption('markerAddress', $court->getAddress());
+
+            $infoWindow = new InfoWindow();
+            $infoWindow->setContent($court->getAddress());
+            $infoWindow->setAutoClose(true);
+            $marker->setInfoWindow($infoWindow);
+
+            $map->addMarker($marker);
+
+            $clickEvent = $this->container->get('ivory_google_map.event');
+            $clickEvent->setInstance($marker->getJavascriptVariable());
+            $clickEvent->setEventName('click');
+            $clickEvent->setHandle('markerClickEventListener');
+
+            $map->getEventManager()->addEvent($clickEvent);
+        }
+
+        return $map;
+    }
 }
